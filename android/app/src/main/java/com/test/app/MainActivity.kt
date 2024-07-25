c973fa81c08214df871d29137651d4a98deca9d1
Oleksandr Volynets <vonswork@gmail.com>
2024-07-25 20:23:44 +0200

    chore: rename app

--- android/app/src/main/java/com/test/app/MainActivity.kt
+++ android/app/src/main/java/com/test/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.xo.tours
+package com.test.app
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/test/app/MainApplication.kt
+++ android/app/src/main/java/com/test/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.xo.tours
+package com.test.app
 
 import android.app.Application
 import com.facebook.react.PackageList
